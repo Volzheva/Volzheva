07cf3208110f19ae5309e65ce988d89c32122340
Maria <47611067+Volzheva@users.noreply.github.com>
2022-10-05 22:52:53 +0300

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,8 +1,9 @@
-alfavit = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+import caesar
+
+
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
-
     >>> encrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> encrypt_vigenere("python", "a")
@@ -10,28 +11,19 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    alfavit = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     ciphertext = ""
-    num_letters = 26
     while len(keyword) < len(plaintext):
         keyword += keyword
     for i in range(0, len(plaintext)):
-        if plaintext[i] in alfavit:
-            shift = ord(keyword[i].lower()) - ord('a')
-            if plaintext[i].lower() == plaintext[i]:
-                start = ord('a')
-            else:
-                start = ord('A')
-            ciphertext += chr(start + (ord(plaintext[i]) + shift - start) % num_letters)
-        else:
-            ciphertext += plaintext[i]
+        shift = ord(keyword[i].lower()) - ord("a")
+        ciphertext += caesar.encrypt_caesar(plaintext[i], shift)
+
     return ciphertext
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     Decrypts a ciphertext using a Vigenere cipher.
-
     >>> decrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> decrypt_vigenere("python", "a")
@@ -39,20 +31,11 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    alfavit = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     plaintext = ""
     num_letters = 26
     while len(keyword) < len(ciphertext):
         keyword += keyword
     for i in range(0, len(ciphertext)):
-        if ciphertext[i] in alfavit:
-            shift = ord(keyword[i].lower()) - ord('a')
-            if ciphertext[i].lower() == ciphertext[i]:
-                start = ord('a')
-            else:
-                start = ord('A')
-            plaintext += chr(start + (ord(ciphertext[i]) - shift - start) % num_letters)
-        else:
-            plaintext += ciphertext[i]
+        shift = ord(keyword[i].lower()) - ord("a")
+        plaintext += caesar.decrypt_caesar(ciphertext[i], shift)
     return plaintext
-
